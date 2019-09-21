26ca814484c1f9a6310ffe284dc6c5c9f6e21fee
posi7790 <posi7790@colorado.edu>
2019-09-21 13:22:26 -0600

    Add Cohort component that builds all cards with peoples information

--- src/components/Cohort.js
+++ src/components/Cohort.js
@@ -2,6 +2,21 @@ import React from 'react'
 import Person from './Person'
 import './Cohort.css'
 
-// COHORT COMPONENT CODE GOES HERE
+const Cohort = ({ cohort }) => {
+  const personCards = cohort.map((person) => (
+    <Person
+      photo={person.photo}
+      name={person.name}
+      quote={person.quote}
+      superlative={person.superlative}
+    />
+  ))
+
+  return (
+    <div className="people-container">
+      {personCards}
+    </div>
+  )
+}
 
 export default Cohort
