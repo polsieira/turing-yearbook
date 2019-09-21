7e44828faac1143baa0439698223dd32d4c616d3
posi7790 <posi7790@colorado.edu>
2019-09-21 13:39:54 -0600

    Fix linting errors in App

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,4 @@
-import React, {
-  Component
-} from 'react'
+import React, { Component } from 'react'
 import Cohort from './Cohort'
 import people from '../data/yearbook-data'
 import './App.css'
@@ -9,20 +7,24 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      staff: people.staff
+      staff: people.staff,
+      students: people.students
     }
   }
 
   render() {
+    const { staff } = this.state
+    const { students } = this.state
+
     return (
       <div className="App">
         <header className="App-header">
           <h1>Turing Yearbook</h1>
         </header>
         <h2>Staff</h2>
-        <Cohort cohort={this.state.staff} />
+        <Cohort cohort={staff} />
         <h2>Students</h2>
-        <Cohort cohort={this.state.students} />
+        <Cohort cohort={students} />
       </div>
     )
   }
--- src/components/Person.js
+++ src/components/Person.js
@@ -2,14 +2,14 @@ import React from 'react'
 import './Person.css'
 
 const Person = ({
-  photo, name, quote, superlative
+ photo, name, quote, superlative 
 }) => (
   <div className="card">
-      <img src={photo} alt="yearbook" height="40px" width="30px" />
-      <h3>{name}</h3>
-      <p>{quote}</p>
-      <p>{superlative}</p>
-    </div>
+    <img src={photo} alt="yearbook" height="40px" width="30px" />
+    <h3>{name}</h3>
+    <p>{quote}</p>
+    <p>{superlative}</p>
+  </div>
 )
 
 export default Person
