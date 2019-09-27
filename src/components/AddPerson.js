75efc947bd064954f21f6ef113f333f6526b6d9c
posi7790 <posi7790@colorado.edu>
2019-09-27 11:26:44 -0600

    Give AddPerson a state

---
+++ src/components/AddPerson.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './AddPerson.css'
+
+class AddPerson extends Component {
+  constructor() {
+    super()
+    this.state = {
+      name: '',
+      quote: '',
+      superlative: ''
+    }
+  }
+}
+
+export default AddPerson
